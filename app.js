14ded62fb37d0a15c93b819f1528a042ec16895f
jeremychristiandi <jeremychristiandi89@gmail.com>
2023-03-09 23:27:12 +0700

    update-4

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ const pb1 = document.querySelector("#pb-1");
 pb1.addEventListener("click", () => {
   window.open("https://github.com/jeremychristiandi/anocamp.git");
 });
+
+const pb2 = document.querySelector("#pb-2");
+
+pb2.addEventListener("click", () => {
+  window.open("https://github.com/jeremychristiandi/kenshin-impact.git");
+});
+
+const pb3 = document.querySelector("#pb-3");
+
+pb3.addEventListener("click", () => {
+  window.open("https://github.com/jeremychristiandi/chatbot-messenger.git");
+});
+
+const pb4 = document.querySelector("#pb-4");
+
+pb4.addEventListener("click", () => {
+  window.open(
+    "https://github.com/jeremychristiandi/karen-virtualassistant.git"
+  );
+});
