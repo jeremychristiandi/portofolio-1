954286e70f823de1cb2f4c8a4949f084a465f288
jeremychristiandi <jeremychristiandi89@gmail.com>
2023-04-11 22:59:57 +0700

    update-11-04-23

--- app.js
+++ app.js
@@ -7,26 +7,32 @@ pb1.addEventListener("click", () => {
 const pb2 = document.querySelector("#pb-2");
 
 pb2.addEventListener("click", () => {
-  window.open("https://github.com/jeremychristiandi/kenshin-impact.git");
+  window.open("https://dicer-jc.vercel.app/");
 });
 
 const pb3 = document.querySelector("#pb-3");
 
 pb3.addEventListener("click", () => {
-  window.open("https://github.com/jeremychristiandi/chatbot-messenger.git");
+  window.open("https://github.com/jeremychristiandi/kenshin-impact.git");
 });
 
 const pb4 = document.querySelector("#pb-4");
 
 pb4.addEventListener("click", () => {
-  window.open(
-    "https://github.com/jeremychristiandi/karen-virtualassistant.git"
-  );
+  window.open("https://github.com/jeremychristiandi/chatbot-messenger.git");
 });
 
 const pb5 = document.querySelector("#pb-5");
 
 pb5.addEventListener("click", () => {
+  window.open(
+    "https://github.com/jeremychristiandi/karen-virtualassistant.git"
+  );
+});
+
+const pb6 = document.querySelector("#pb-6");
+
+pb6.addEventListener("click", () => {
   window.open("https://github.com/jeremychristiandi/flight-visualization.git");
 });
 
